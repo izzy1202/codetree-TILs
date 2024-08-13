639e1ee030ad5d1d22b70d658b695bdfb05cd996
Jieun Lee <leejieun8765@gmail.com>
2024-08-13 21:06:50 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240813/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp1 = a;
+        int temp2 = b;
+        a = c;
+        System.out.println(a);
+        b = temp1;
+        System.out.println(b);
+        c = temp2;
+        System.out.println(c);
+        
+
+
+    }
+}
