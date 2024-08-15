5f62a606f172c62a53450876f3a5497c58ec4098
Jieun Lee <leejieun8765@gmail.com>
2024-08-15 18:08:51 +0900

    Title: 점수 입출력, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240815/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner 선언
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+        System.out.println("Your score is "+n+" point.");
+    }
+}
