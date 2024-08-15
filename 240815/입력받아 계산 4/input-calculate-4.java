b28cb8f52e1f73a3a9e57191c685dc4176cd067f
Jieun Lee <leejieun8765@gmail.com>
2024-08-15 18:31:08 +0900

    Title: 입력받아 계산 4, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240815/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력 받기위해 Scanner 선언
+        Scanner sc = new Scanner(System.in);
+
+        // 정수형 변수 선언
+        int a;
+
+        // 정수를 입력받아 a에 저장
+        a = sc.nextInt();
+
+        // 입력값에 2 곱한 값 출력
+        System.out.print(a*2);
+
+
+    }
+}
